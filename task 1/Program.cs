0b64cf9f326e938fbdb1b6d5cabd407c11e88f90
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-26 16:37:22 +0300

    task 1 solved

---
+++ task 1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int InputArraySize(string msg)
+{
+    System.Console.WriteLine("Input array size");
+    if(int.TryParse(Console.ReadLine(), out int value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Only integer pls");
+    return 0;
+}
+
+int[] ArrayCreation(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+void ArrayPrint(int[] array)
+{
+    System.Console.Write(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.Write(", " + array[i]);
+    }
+    System.Console.WriteLine();
+}
+
+int EvenNumAmmount(int[] array)
+{
+    int ammount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        {
+            ammount +=1;
+        }
+    }
+    return ammount;
+}
+
+int size = InputArraySize("Input array size");
+int[] array = ArrayCreation(size);
+ArrayPrint(array);
+System.Console.WriteLine($"Ammount of even numbers in array is equal {EvenNumAmmount(array)}");
